97a675e882213c8a35415384b8cbe65e442c7e8f
Arnaud <arnaud.nommay@orange.fr>
2023-03-06 16:48:37 +0100

    Project initialization

---
+++ SwiftUIWorld/SwiftUIWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWorldApp.swift
+//  SwiftUIWorld
+//
+//  Created by Arnaud NOMMAY on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
